01b6494fce611adb64d27b42c069aba657d7b226
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-17 23:50:58 +0200

    Added some constants from libcpuid_constants.h

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,5 +1,10 @@
 use libc::{c_int, c_char};
 
+pub static MAX_CPUID_LEVEL: uint = 32u;
+pub static MAX_EXT_CPUID_LEVEL: uint = 32u;
+pub static MAX_INTELFN4_LEVEL: uint = 4u;
+pub static MAX_INTELFN11_LEVEL: uint = 4u;
+
 #[link(name = "cpuid")]
 extern {
     pub fn cpuid_present() -> c_int;

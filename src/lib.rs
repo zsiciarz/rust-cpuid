4b5e873698078e8a8d792021ed0544e3e79bd313
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-18 00:00:19 +0200

    Started work on identify() function.

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,20 @@ pub fn error() -> String {
     error_string.as_str().unwrap().to_string()
 }
 
+pub fn identify() -> bool {
+    let mut raw = ffi::cpu_raw_data_t {
+        basic_cpuid: [0, ..ffi::MAX_CPUID_LEVEL * 4u],
+        ext_cpuid: [0, ..ffi::MAX_EXT_CPUID_LEVEL * 4u],
+        intel_fn4: [0, ..ffi::MAX_INTELFN4_LEVEL * 4u],
+        intel_fn11: [0, ..ffi::MAX_INTELFN11_LEVEL * 4u],
+    };
+    let result = unsafe {
+        ffi::cpuid_get_raw_data(&mut raw)
+    };
+    // TODO: use raw in the call to cpu_identify
+    result == 0
+}
+
 #[test]
 fn test_is_present() {
     assert!(is_present());

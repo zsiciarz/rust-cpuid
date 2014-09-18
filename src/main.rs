9b69ff9ab113aa6468d56977ec9878983a455019
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-18 20:07:59 +0200

    A bit more meaningful main program.

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,14 @@ extern crate cpuid;
 fn main () {
     println!("cpuid is present: {}", cpuid::is_present());
     println!("cpuid version: {}", cpuid::version());
-    println!("cpuid error: {}", cpuid::error());
+    let info = cpuid::identify();
+    match info {
+        Err(err) => println!("cpuid error: {}", err),
+        Ok(info) => {
+            println!("Found: {} CPU", info.vendor);
+            println!("Processor model is: {}", info.codename);
+            println!("The full brand string is: {}", info.brand);
+            println!("The processor has {} cores and {} logical processors", info.num_cores, info.num_logical_cpus);
+        }
+    }
 }

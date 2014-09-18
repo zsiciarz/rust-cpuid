eb6d8e692fa5cf94fdc074a7416e6a9fbbb6b83f
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-18 18:47:35 +0200

    Added cpu_id_t definition.

--- src/ffi.rs
+++ src/ffi.rs
@@ -1,4 +1,4 @@
-use libc::{c_int, c_char, uint32_t};
+use libc::{c_int, c_char, uint8_t, uint32_t, int32_t};
 
 pub static MAX_CPUID_LEVEL: uint = 32u;
 pub static MAX_EXT_CPUID_LEVEL: uint = 32u;
@@ -17,7 +17,34 @@ pub struct cpu_raw_data_t {
     pub intel_fn11: [[uint32_t, ..MAX_INTELFN11_LEVEL], ..4u],
 }
 
-// TODO: definition for struct cpu_id_t
+#[repr(C)]
+pub struct cpu_id_t {
+    pub vendor_str: [c_char, ..VENDOR_STR_MAX],
+    pub brand_str: [c_char, ..BRAND_STR_MAX],
+    pub vendor: int32_t,
+    pub flags: [uint8_t, ..CPU_FLAGS_MAX],
+    pub family: int32_t,
+    pub model: int32_t,
+    pub stepping: int32_t,
+    pub ext_family: int32_t,
+    pub ext_model: int32_t,
+    pub num_cores: int32_t,
+    pub num_logical_cpus: int32_t,
+    pub total_logical_cpus: int32_t,
+    pub l1_data_cache: int32_t,
+    pub l1_instruction_cache: int32_t,
+    pub l2_cache: int32_t,
+    pub l3_cache: int32_t,
+    pub l1_assoc: int32_t,
+    pub l2_assoc: int32_t,
+    pub l3_assoc: int32_t,
+    pub l1_cacheline: int32_t,
+    pub l2_cacheline: int32_t,
+    pub l3_cacheline: int32_t,
+    pub cpu_codename: [c_char, ..64],
+    pub sse_size: int32_t,
+    pub detection_hints: [uint8_t, ..CPU_HINTS_MAX],
+}
 
 #[link(name = "cpuid")]
 extern {

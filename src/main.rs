082d57c214253c548a3d41eff8e7868990226bcb
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-21 16:49:50 +0200

    Updated main.rs from readme.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@ extern crate cpuid;
 fn main () {
     println!("cpuid is present: {}", cpuid::is_present());
     println!("cpuid version: {}", cpuid::version());
-    let info = cpuid::identify();
-    match info {
-        Err(err) => println!("cpuid error: {}", err),
+    match cpuid::identify() {
         Ok(info) => {
-            println!("Found: {} CPU", info.vendor);
-            println!("Processor model is: {}", info.codename);
+            println!("Found: {} CPU, model: {}", info.vendor, info.codename);
             println!("The full brand string is: {}", info.brand);
             println!("The processor has {} cores and {} logical processors", info.num_cores, info.num_logical_cpus);
-            match cpuid::clock_frequency() {
-                Some(frequency) => println!("CPU speed: {} MHz", frequency),
-                None => println!("Couldn't get CPU speed."),
-            };
-        }
-    }
+            if info.has_feature(cpuid::FloatingPointUnit) {
+                println!("Floating point unit available");
+            }
+        },
+        Err(err) => println!("cpuid error: {}", err),
+    };
+    match cpuid::clock_frequency() {
+        Some(frequency) => println!("CPU speed: {} MHz", frequency),
+        None => println!("Couldn't get CPU speed."),
+    };
 }
